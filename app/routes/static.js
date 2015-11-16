30c5f78f7a49322dd8da51e0491d3b48ec30f293
Sam Hill <sam_hill@intuit.com>
2015-11-15 17:16:46 -0800

    can view the names of the movements in the workout now, woohoo

--- app/routes/static.js
+++ app/routes/static.js
@@ -6,15 +6,16 @@ module.exports = function(app, passport) {
 	// ROUTINES LIST VIEW =======================
 	app.get('/routines', isLoggedIn, function(req, res) {
 		res.render('routines.ejs', {
-			user : req.user
+			user : req.user,
+			myRoutines: req.user.data.myRoutines
 		});
 	});
 	
 	// ROUTINE DETAIL VIEW =======================
-	app.get('/routine/:position', isLoggedIn, function(req, res) {
+	app.get('/routine/:position/workout/:position2', isLoggedIn, function(req, res) {
 		res.render('routine.ejs', {
 			user : req.user,
-			routine : req.user.data.myRoutines[0]
+			routine : req.user.data.myRoutines[req.params['position']].workouts[req.params['position2']]
 		});
 	});
 	
